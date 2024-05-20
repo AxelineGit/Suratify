20ca8c4df91a3ceb42b00a1f4d64c2df493720ba
AxelineGit <saputrovalentinus@gmail.com>
2024-05-20 10:53:18 +0700

    db

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,33 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/suratdesa1', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
+
+class Database {
+    constructor() {
+      this.connection = null;
+    }
+    
+    static getInstance() {
+      if (!Database.instance) {
+        Database.instance = new Database();
+      }
+      return Database.instance; 
+    }
+  
+    connect() {
+        mongoose.connect('mongodb://127.0.0.1:27017/suratdesa1', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        this.connection = 'Connected'; 
+    }
+  }
+  
+  const db1 = Database.getInstance();
+  const db2 = Database.getInstance();
+  
+  console.log(db1 === db2); // true
+  
+  db1.connect(); 
+  
+  console.log(db1.connection); // 'Connected'
+  console.log(db2.connection); // 'Connected'
